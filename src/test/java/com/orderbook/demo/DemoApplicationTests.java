e8b66ac7117bcd294ad9beba6769e65dabacfb86
Orz.KR <25887708@qq.com>
2020-03-25 16:50:36 +0800

    add docker file, support run the service in docker

--- src/test/java/com/orderbook/demo/DemoApplicationTests.java
+++ src/test/java/com/orderbook/demo/DemoApplicationTests.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import com.orderbook.management.OrderApplication;
+
 @SpringBootTest
 public class DemoApplicationTests {
 
